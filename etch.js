e6b406c7f2f2efe6ce43d1cc8073b5c0be4abac0
Max Lopez <familiacachorin@gmail.com>
2023-06-28 12:47:30 -0700

    Implemented the hover feature.

--- etch.js
+++ etch.js
@@ -1,16 +1,21 @@
 const container = document.querySelector('.container');
 
+
 //creating grid
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < 16; i++) {
     const row = document.createElement('div');
     row.setAttribute('class', 'row');   
-    for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 16; j++) {
+        let id = "Box:" + i + "&" + j;
         const box = document.createElement('div'); 
-        box.setAttribute('class', 'box');  
+        box.setAttribute('class', 'box'); 
+        box.setAttribute('id', id);   
         row.appendChild(box)  
+        box.addEventListener('mouseover', changeBox);
     }
-
-    
-    
     container.appendChild(row);
+}
+//changing color of box
+function changeBox(e) {
+    e.target.style.background = 'white';
 }
