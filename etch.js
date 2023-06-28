2eef4016011c6d610832649c9965cb6ba9951703
Max Lopez <familiacachorin@gmail.com>
2023-06-28 13:41:08 -0700

    add button to set size of grid, nonfunctioning.

--- etch.js
+++ etch.js
@@ -1,11 +1,17 @@
+//elements
 const container = document.querySelector('.container');
+const sizeButton = document.getElementById('setSizeButton');
+let sidesSize = 16;
+sizeButton.addEventListener('click', test);
+
 
 
 //creating grid
-for (let i = 0; i < 16; i++) {
+
+for (let i = 0; i < sidesSize; i++) {
     const row = document.createElement('div');
     row.setAttribute('class', 'row');   
-    for (let j = 0; j < 16; j++) {
+    for (let j = 0; j < sidesSize; j++) {
         let id = "Box:" + i + "&" + j;
         const box = document.createElement('div'); 
         box.setAttribute('class', 'box'); 
@@ -18,4 +24,19 @@ for (let i = 0; i < 16; i++) {
 //changing color of box
 function changeBox(e) {
     e.target.style.background = 'white';
+}
+function getUserInput() {
+    let num = -1; 
+    while (num <= 0 && num > 100) {
+        num = prompt("Please a number", "number 1 -100");
+    }
+    return num;
+
+}
+function setSidesSize (size) {
+    sidesSize = size;
+}
+function test () {
+    console.log("Hello");
+    let sidesSize  = prompt("Please a number", "number 1 -100");
 }
